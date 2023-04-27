094c3090d38b1da77a3c4838b3f598a528f86c63
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2023-04-27 19:04:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# aula-2023-04-20
+# MVC
+
+Model (modelo)
+View (visão/apresentação)
+Controller (controlador)
+
+Existem outros:
+
+MVVM: Model View ViewModel
+MVP: Model View Presenter
---
+++ src/main/java/br/edu/ifrs/riogrande/tads/cobaia/entity/Aluno.java
@@ -0,0 +1,83 @@
+package br.edu.ifrs.riogrande.tads.cobaia.entity;
+
+import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno { // Java Bean
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // sequencial
+  private Long id;
+
+  @Column(length = 50, nullable = false)
+  @JsonProperty("nomeCompleto") // body/endpoint
+  private String nome;
+
+  // Sem length considera 255
+  @Column(length = 100, nullable = false, unique = true)
+  private String email;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+  
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Aluno other = (Aluno) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Aluno [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+  }
+}
